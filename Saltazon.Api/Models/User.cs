32b6484cfab61ea991cf692d7d7a4c47586c88fb
swathi <swathysidharthan@gmail.com>
2023-02-07 11:34:32 +0100

    add usermodel

---
+++ Saltazon.Api/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Saltazon.Api.Models
+{
+    public class User
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("email")]
+        public string Email { get; set; }
+
+        [JsonPropertyName("password")]
+        public string Password { get; set; }
+
+        [JsonPropertyName("role")]
+        public string Role { get; set; }
+
+        [JsonPropertyName("uniqueStoreId")]
+        public string UniqueStoreId { get; set; }
+
+    }
+}
